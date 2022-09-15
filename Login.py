25b1eadced78b5149c75886db9c4fdfb86063ca9
Vinicius Guilherme Caetano <vinicaet97@gmail.com>
2022-09-15 19:21:33 -0300

    Criação de tela de Login

---
+++ Login.py
@@ -0,0 +1,19 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#Layout
+sg.theme('Material2')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuario')],
+    [sg.Text('Senha'),sg.Input(key='senha',password_char='*')],
+    [sg.Button('Entrar')]
+]
+#Janela
+janela = sg.Window('Tela de Login', layout)
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores ['usuario'] == 'Teste' and valores['senha'] == 'Teste'   :
+            print('Bem vindo!')
