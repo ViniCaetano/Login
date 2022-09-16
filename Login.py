fa754dfef6826cc5bb4b720d29b4d8dbf22b0f96
Vinicius Guilherme Caetano <vinicaet97@gmail.com>
2022-09-16 18:09:40 -0300

    Conexão BD
    Conexão ao BD SQL Server
    Mensagens de erro em caso de falha no login

--- Login.py
+++ Login.py
@@ -1,11 +1,20 @@
+import pyodbc
 from PySimpleGUI import PySimpleGUI as sg
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-6UP649V;"
+    "Database=Login;"
+)
+conexao = pyodbc.connect(dados_conexao)
 
 #Layout
 sg.theme('Material2')
 layout = [
     [sg.Text('Usuário'),sg.Input(key='usuario')],
     [sg.Text('Senha'),sg.Input(key='senha',password_char='*')],
-    [sg.Button('Entrar')]
+    [sg.Button('Entrar')], 
+    [sg.Button('Cadastrar')]
 ]
 #Janela
 janela = sg.Window('Tela de Login', layout)
@@ -15,5 +24,10 @@ while True:
     if eventos == sg.WINDOW_CLOSED:
         break
     if eventos == 'Entrar':
-        if valores ['usuario'] == 'Teste' and valores['senha'] == 'Teste'   :
+        if valores ['usuario'] == 'teste' and valores ['senha'] == '123456':
             print('Bem vindo!')
+        elif valores ['usuario'] == 'teste' and valores ['senha'] != '123456':
+            print('Senha incorreta.')
+        elif valores ['usuario'] != 'teste':
+            print('Usuário Incorreto/Não Cadastrado')
+    
